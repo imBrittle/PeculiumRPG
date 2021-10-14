614aa0ffa31afd4895f20320ed9142d82273b655
twitchBrittle <45422296+twitchBrittle@users.noreply.github.com>
2021-10-14 14:49:59 +0800

    Stat Addition and Inventory Adjustments

--- #BrittleRPG.py
+++ #BrittleRPG.py
@@ -1,6 +1,7 @@
-#AstrusRPG
+# Astrus RPG Game
 import random
 import time
+from Classes import itemArcaneCrystal, itemHealingStone, spellBurn, spellShock, spellGale, spellSplash
 
 print("==================")
 print("==  WELCOME TO  ==")
@@ -14,19 +15,30 @@ time.sleep(2)
 username = input("Enter your username: ")
 print("Your username has been set to: " + username)
 print()
-character = input("Choose your element (Pyro, Capacita, Atmos, Hydro): ")
-print("Your element has been set to: " + character)
+chooseElement = True
+while chooseElement == True:
+    character = input("Choose your element (Pyro, Capacita, Atmos, Hydro): ")
+    if character in ["Pyro", "pyro", "Capacita", "capacita", "Atmos", "atmos", "Hydro", "hydro"]:
+        print("Your element has been set to: " + character)
+        chooseElement = False
+    elif character in ["Dev", "dev", "Developer", "developer"]:
+        print("Developer mode has been enabled.")
+        chooseElement = False
+    else:
+        print("Element not recognised. Please select an existing element.")
+        print()
+
 print()
-
-print("Applying Settings...")
-
+print("Loading...")
 time.perf_counter()
+
 #Game Lists
 pyroSpells = ["Burn"]
 capacitaSpells = ["Shock"]
 atmosSpells = ["Gale"]
 hydroSpells = ["Splash"]
-items = ["Healing Potion", "Arcane Crystal"]
+otherSpells = ["Dev"]
+items = [itemArcaneCrystal(), itemHealingStone()]
 
 #Character Settings
 hp = 100
@@ -36,8 +48,21 @@ maxEnergy = 100
 mana = 100
 maxMana = 100
 inventory = []
+scroll = []
 exploreCount = 0
 sleepCount = 0
+
+if character in ["Pyro", "pyro"]:
+    scroll.append("Burn")
+elif character in ["Capacita", "capacita"]:
+    scroll.append("Shock")
+elif character in ["Atmos", "atmos"]:
+    scroll.append("Gale")
+elif character in ["Hydro", "hydro"]:
+    scroll.append("Hydro")
+else:
+    scroll.append("Admin")
+        
 
 time.perf_counter()
 print(f"Done! Loading took {time.perf_counter()} nanoseconds.")
@@ -77,14 +102,16 @@ while ingame == True:
                     print()
                     print(f"{bagDecision}:")
                     print()
-                    print("- Use")
-                    print("- Discard")
-                    print("- Cancel")
+                    print("1) Use")
+                    print("2) Discard")
+                    print("3) Cancel")
                     print()
                     bagSubDecision = input(">>> ")
                     if bagSubDecision == ["Use", "use"]:
                         print()
-                        #-- Conditions for item use
+                        inventory[0].use()
+                        inventory.remove(bagDecision)
+
 
             elif inventoryCount > 1:
                 print(f"You have {inventoryCount} items in your bag:")
@@ -105,10 +132,16 @@ while ingame == True:
 
         elif actionDecision in ["3", "Change Spells", "Change spells", "change spells", "Spells", "spells"]:
             print()
+            
 
         elif actionDecision in ["4", "Check Stats", "Checks Stats", "check stats", "Stats", "stats"]:
             print()
-
+            print("Stats:")
+            print(f"HP: {hp}/{maxHp}")
+            print(f"Energy: {energy}/{maxEnergy}")
+            print(f"Mana: {mana}/{maxMana}")
+            print()
+ 
         elif actionDecision in ["5", "Sleep", "sleep"]:
             print()
             print("Sleeping...")
@@ -117,6 +150,7 @@ while ingame == True:
             if sleepCount == 0:
                 print("I close my eyes and think about this intriguing place. I'm not sure how I got here, but I'm going to make the most of it and do whatever I can to get out. I ponder on the idea of improving my spells, and I think I may've experienced a breakthrough. The Arcane Stone that I found when I arrived has provided me with some basic information as to how spells work, and I may be able to put it to use to improve my spells.")
                 inventory.append("Arcane Crystal")
+                print("Received x1 Arcane Crystal")
 
                 hp = maxHp
                 energy = maxEnergy
